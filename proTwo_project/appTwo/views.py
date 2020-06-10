464909bfd7cfdd36e091d28f74a0661046b483ac
kimhi <60768642+Einsicht1@users.noreply.github.com>
2020-06-10 10:48:27 +0900

    Django_practice_user_signup

--- proTwo_project/appTwo/views.py
+++ proTwo_project/appTwo/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from appTwo.models import User
-
+from appTwo.forms import NewUserForm
 
 
 
@@ -10,9 +9,16 @@ def index(request):
     return render(request, 'appTwo/index.html')
 
 def users(request):
+    form = NewUserForm()
 
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users': user_list}
-    return render(request, 'appTwo/users.html', context=user_dict)
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
 
-    
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("ERROR FORM INVALID")
+
+    return render(request, 'appTwo/users.html' ,{'form':form})
+
